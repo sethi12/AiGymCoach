2cd2ae1dc35ada6b69ef9797749aa57a8a664e5f
Chaitanya Sethi <sethichaitanya08@gmail.com>
2026-06-02 02:03:42 +0530

    chnged for streamlit

--- services/firebase/firebase_service.py
+++ services/firebase/firebase_service.py
@@ -1,22 +1,41 @@
-import os
-import json
+# import os
+# import json
+# import firebase_admin
+
+# from dotenv import load_dotenv
+# from firebase_admin import credentials, firestore
+
+# load_dotenv()
+
+# firebase_credentials = json.loads(
+#     os.getenv("FIREBASE_CREDENTIALS")
+# )
+
+# if not firebase_admin._apps:
+
+#     cred = credentials.Certificate(
+#         firebase_credentials
+#     )
+
+#     firebase_admin.initialize_app(cred)
+
+# db = firestore.client()
+
+
+
+import streamlit as st
 import firebase_admin
 
-from dotenv import load_dotenv
 from firebase_admin import credentials, firestore
 
-load_dotenv()
-
-firebase_credentials = json.loads(
-    os.getenv("FIREBASE_CREDENTIALS")
+firebase_credentials = dict(
+    st.secrets["firebase"]
 )
 
 if not firebase_admin._apps:
-
     cred = credentials.Certificate(
         firebase_credentials
     )
-
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
