5b5bf578c0deceeee35f260de02fadc4c758d83c
Chaitanya Sethi <sethichaitanya08@gmail.com>
2026-07-05 02:55:07 +0530

    added one tab changing of link button

--- main.py
+++ main.py
@@ -124,11 +124,25 @@ section[data-testid="stSidebar"] {
     
     with st.sidebar:
         st.title("🏋️‍♂️ Apna AI Coach")
-        st.link_button(
-        "⬅ Back to Dashboard",
-        "https://aigym-web-landing-gym.vercel.app/dashboard",
-        use_container_width=True,
-            )
+        st.markdown(
+    """
+    <a href="https://aigym-web-landing-gym.vercel.app/dashboard" target="_self"
+       style="
+            display:block;
+            width:100%;
+            padding:0.6rem 1rem;
+            text-align:center;
+            text-decoration:none;
+            border:1px solid #4a4a4a;
+            border-radius:8px;
+            background-color:#262730;
+            color:white;
+            font-weight:600;">
+        ⬅ Back to Exercises
+    </a>
+    """,
+    unsafe_allow_html=True,
+        )
         if st.session_state.username:
             st.caption(f"👤 Login as {st.session_state.username}")
             
